9adb99e44aa3437cf44035af9eab214e62392415
Marissa Karpack <mnk5@rams.colostate.edu>
2019-01-24 12:42:15 -0700

    Add plots by stream order
    Assess hydrologic alteration metics by stream order for both stream segments and mean with FP segments

--- HydAlterationComparison.R
+++ HydAlterationComparison.R
@@ -34,4 +34,39 @@ for (i in 1:length(HydAlt)){
       xlab ='Aggregating Method', 
       ylab = 'Hydologic Alteration',
       main = names[i])
- }
+}
+
+#############################################
+## look at hyd alteration metrics by order
+
+streamfile <- "C:/Users/mnk5/Documents/GIS/DATA/Hyd_Alteration/ByHUC12/attributetable.txt"
+
+df2 <- read.csv(streamfile)
+df2 <- df2[, c("pnMH20", "pnFH1", "pnFH6", "pnFH7", "pnDH1", "pnDH2","pnDH3","pnDH4","pnDH5","pnDH15",'StrmOrder')]
+
+# Create boxlpots for each of 10 Hyd Alt metrics for stream segments by Stream Order
+boxplots_SO <- list()
+for (i in 1:10){
+  metric <- names[i]
+  boxplots_SO[[i]] <-  boxplot(df2[,i] ~ StrmOrder, data = df2, 
+                            xlab ='Stream Order', 
+                            ylab = 'Hydologic Alteration',
+                            main = sprintf('%s by Stream Segment', metric))
+}
+
+#############################################
+# Repeat for mean Hyd Alt by Stream order for FP
+
+meandata <- df[,c(20:29, 54)]
+# set -999 values from GIS to NA 
+meandata[meandata == -999] <- NA
+
+## Create boxlpots for each of 10 Hyd Alt metrics for FP segments by Stream Order
+boxplots_FP_SO <- list()
+for (i in 1:10){
+  metric <- names[i]
+  boxplots_FP_SO[[i]] <-  boxplot(meandata[,i] ~ StrmOrder, data = meandata, 
+                               xlab ='Stream Order', 
+                               ylab = 'Hydologic Alteration',
+                               main = sprintf('%s by FP Segment', metric))
+}
