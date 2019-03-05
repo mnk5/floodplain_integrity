99d55f7d5d55197fc0e899073d219ad09b77dc0b
Marissa Karpack <mnk5@rams.colostate.edu>
2019-03-05 16:30:00 -0700

    Plot graphs of data distributions
    Analyze input datasets with histograms and boxplots to identify errors and look at data distribution

--- IndexCalculation.R
+++ IndexCalculation.R
@@ -16,12 +16,18 @@ basepath <- "C:/Users/mnk5/Documents/floodplain_integrity"
 # Load csv file of stressor data from "CorrelationAnalysis.R" script output
 data.path <- paste(basepath, "/Outputs/Combined_Data.csv", sep="")
 all.data <- read.csv(data.path)
+data.names <- colnames(all.data)
 
+# Convert HUC-12 from numeric to character
+all.data$HUC12 <- as.character(all.data$HUC12)
 
 # boxplots to look at range of data
 
 for (i in 4:ncol(all.data)){
-  hist(all.data[,i])
+  hist(all.data[,i], main = data.names[i])
+  boxplot(all.data[,i], main = data.names[i])
+  text(y=fivenum(all.data[,i]), labels = round(fivenum(all.data[,i]), digits = 2), x = 0.75)
   
 }
+
 
