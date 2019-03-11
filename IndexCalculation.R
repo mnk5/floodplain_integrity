47ab4bcfb3deceb13732c2e5bd6c7ebfb91f74cf
Marissa Karpack <mnk5@rams.colostate.edu>
2019-03-11 13:36:29 -0600

    Calculate Function IFI
    choose stressors and calculate IFI by function using mean of stressors

--- IndexCalculation.R
+++ IndexCalculation.R
@@ -30,4 +30,78 @@ for (i in 4:ncol(all.data)){
   
 }
 
+#####################
+# Get only final stressors and HUC-12 Identifier into df
+HUC12 <- all.data$HUC12
+keep.columns <- c("Agriculture", "Buildings", "Ditches", "Developed", "ForestLoss",
+                  "Impervious", "LeveedArea", "NonNativeVeg", "Roads_Rail","Wells", "MH20")
+stressors <- all.data[, keep.columns]
+
+# adjust stressors that are not 0 to 1 
+stressors$Impervious <- stressors$Impervious/100 # convert percent to decimal
+
+# Scale count and line denisty by max value observed
+stressors$Ditches <- stressors$Ditches/max(stressors$Ditches)
+stressors$Roads_Rail <- stressors$Roads_Rail/max(stressors$Roads_Rail)
+stressors$Wells <- stressors$Wells/max(stressors$Wells)
+
+# Scale buildings to max building density in CO
+stressors$Buildings <- stressors$Buildings/max(stressors$Buildings)
+
+# Compare all measures
+boxplot(stressors, use.cols = TRUE)
+
+
+# Make neagtive 
+stressors.neg <- 1-stressors
+boxplot(stressors.neg, use.cols = T)
+
+#####################
+# Calculate functions as average of stressors
+
+# choose datasets by function
+
+# Flood reduction
+FR.stressors <- c("Buildings", "Roads_Rail", "ForestLoss", "Developed", "Wells", "LeveedArea")
+# Groundwater regulation
+GW.stressors <- c("Impervious", "Ditches", "Agriculture", "ForestLoss")
+# Sediment Flux
+SF.stressors <- c("MH20", "Agriculture", "Roads_Rail", "ForestLoss")
+# Organics and Solute regulation
+OS.stressors <- c("MH20", "ForestLoss", "Impervious") # Roads_Rail not included b/c of high correlation
+# Habitat provisioning 
+HP.stressors <- c("Roads_Rail", "MH20", "NonNativeVeg", "Developed", "Agriculture", "ForestLoss")
+
+data.byfunction <- list(FR.stressors, GW.stressors, SF.stressors, OS.stressors, HP.stressors)
+
+
+# Compute Index as average of stressors for each function
+Function.Index <- data.frame(matrix(NA, nrow = nrow(stressors.neg), ncol = length(data.byfunction)))
+
+for (i in 1:length(data.byfunction)) {
+  Function.Index[,i] <- rowMeans(stressors.neg[,data.byfunction[[i]]])
+}
+
+colnames(Function.Index) <- c("Floods", "Groudwater", "Sediment", 
+                              "Organic/Solutes", "Habitat")
+
+
+# plot boxplot of index by function
+function.plot <- ggplot(stack(Function.Index), aes(x = ind, y = values)) + 
+  geom_boxplot() +
+  scale_y_continuous(limits = c(0,1)) +
+  xlab("Floodplain Function") +
+  ylab("Integrity Index") +
+  ggtitle("Index of Floodplain Integrity by Function")
+function.plot
+
+# Compute overall Index of floodplain Integrity
+
+
+# Compute index using percentiles
+
+
+
+
+
 
