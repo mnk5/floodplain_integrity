f2d0ec1bc0ba743f23ca6699d09d358e476bdd60
Marissa Karpack <mnk5@rams.colostate.edu>
2019-04-23 15:26:48 -0600

    Add map of IFI by HUC-12
    Added mapping of IFI values to HUC-12 to visualize results

--- ResultsMapping.R
+++ ResultsMapping.R
@@ -23,17 +23,20 @@ setwd("C:/Users/mnk5/Documents/floodplain_integrity")
 # read in files
 floodplain <- readOGR(dsn = "RawData/SpatialData", layer = "CO_FP_IFI")
 CO.boundary <- readOGR(dsn = "RawData/SpatialData", layer = "CO_StateBoundary_UTM")
-CO.HUC12 <- readOGR(dsn = "RawData/SpatialData", layer = "CO_HUC12")
+CO.HUC12 <- readOGR(dsn = "RawData/SpatialData", layer = "CO_HUC12_IFI")
 # NHD v1 segments order 4 and larger in Colorado
 CO.rivers <- readOGR(dsn = "RawData/SpatialData", layer = "NHDv1_Order4_CO")
 
 # Clean data
 floodplain$HUC12 <- as.character(floodplain$HUC12)
-
+CO.HUC12$HUC12 <- as.character(CO.HUC12$HUC12)
 
 # transform for ggplot
 floodplain_tidy <- tidy(floodplain, region = "HUC12")
 floodplain.df <- left_join(floodplain_tidy, floodplain@data, by = c("id" = "HUC12"))
+
+HUC12_tidy <- tidy(CO.HUC12, region = "HUC12")
+HUC12.df <- left_join(HUC12_tidy, CO.HUC12@data, by = c("id" = "HUC12"))
 
 CO.boundary@data$id <- row.names(CO.boundary@data)
 CO.boundary_tidy <- tidy(CO.boundary, region = 'id')
@@ -66,19 +69,39 @@ map <- ggplot(data = floodplain.df, aes(x = long, y = lat, group = group)) +
 map
 
 # Overall IFI
+
 map1 <- ggplot(data = floodplain.df, aes(x = long, y = lat, group = group, fill = IFI_geomea)) + 
   geom_polygon(data = CO.boundary_tidy, aes(x = long, y = lat, group = group), fill = "grey93") +
   geom_polygon(data = floodplain.df, aes(x = long, y = lat, group = group, fill = IFI_geomea)) +
   coord_equal() +
-  scale_fill_gradientn(colours = c("chocolate4", "wheat1" ,"darkcyan"), breaks = seq(0, 1, by = 0.1))
+  # coord_fixed(ratio = 1, xlim = xlimits, ylim = ylimits) +
+  scale_fill_gradientn(colours = c("chocolate4", "wheat1" ,"darkcyan"), breaks = seq(0, 1, by = 0.2))
 map1 <- map1 + labs(x = NULL, y = NULL, fill = "IFI")
 map1 <- map1 + theme_minimal(base_size = 14) + 
-  theme(legend.text = element_text(size = 8)) +
+  theme(legend.text = element_text(size = 14)) +
   theme(axis.text=element_blank()) +
   theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 
 map1
 
+# Overall IFI mapped to HUC-12 units
+
+map2 <- ggplot(data = HUC12.df, aes(x = long, y = lat, group = group)) + 
+  geom_polygon(data = HUC12.df, color = "grey27", size = 0.1, aes(x = long, y = lat, group = group, fill = IFI_geomea)) +
+  geom_polygon(data = CO.boundary_tidy, aes(x = long, y = lat, group = group), 
+               fill = NA, color = "black", size = 1.5) +
+  geom_path(data = CO.rivers_tidy, aes(x = long, y = lat, group = group), color = "navy", size = 1) +
+  coord_equal() +
+  # coord_fixed(ratio = 1, xlim = xlimits, ylim = ylimits) +
+  scale_fill_gradientn(colours = c("chocolate4", "wheat1" ,"darkcyan"), breaks = seq(0, 1.0, by = 0.2), 
+                       labels = c("0.0","0.2", "0.4", "0.6", "0.8", "1.0"), limits = c(0,1)) +
+  labs(x = NULL, y = NULL, fill = "IFI") +
+  theme_minimal(base_size = 12) + 
+  theme(legend.text = element_text(size = 12)) +
+  theme(axis.text=element_blank()) +
+  theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+
+map2
 # # All IFI by function
 
 # Mapping zoomed in area IFI by function
@@ -86,6 +109,7 @@ fp.df <- melt(floodplain.df, id = 1:9, measure = 13:18)
 levels(fp.df$variable) = c("Flood Reduction", "Groundwater Storage", "Sediment Regulation",
                            "Organics/Solutes Regulation", "Habitat Provision", "Overall IFI")
 
+# change projection to WGS84
 
 
 map7 <- ggplot(data = fp.df, aes(x = long, y = lat, group = group, fill = value)) + 
@@ -97,11 +121,12 @@ map7 <- ggplot(data = fp.df, aes(x = long, y = lat, group = group, fill = value)
   facet_wrap(~ variable, ncol = 3) +
   scale_fill_gradientn(colours = c("chocolate4", "wheat1" ,"darkcyan"), breaks = seq(0, 1, by = 0.2)) +
   labs(x = NULL, y = NULL, fill = "IFI") +
-  theme_minimal(base_size = 12) + 
+  theme_minimal(base_size = 16) + 
   theme(panel.background = element_rect(fill = "grey93"),
         panel.border = element_rect(fill = NA, colour = "black"),
         legend.position = "bottom",
-        legend.text = element_text(size = 8),
-        axis.text=element_blank(),
-        panel.grid.major = element_blank(), panel.grid.minor = element_blank())
+        legend.text = element_text(size = 12),
+        legend.key.width = unit(1, "cm"))
+        # axis.text=element_blank(),
+        # panel.grid.major = element_blank(), panel.grid.minor = element_blank())
 map7
